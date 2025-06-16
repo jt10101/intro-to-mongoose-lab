0b02b60e0ddd5c11e21e5e9b16a3ce93ad5ae068
Jacob Tham <jacob@mac.lan>
2025-06-16 21:44:18 +0800

    code refactor with while loop

--- app.js
+++ app.js
@@ -21,85 +21,76 @@ const connect = async () => {
 };
 
 const runQueries = async () => {
+  let isActive = true;
   console.log("Queries running.");
   mongoose.set("debug", true);
 
-  // Prompt
-  const prompt = require("prompt-sync")();
-  console.log(
-    "\nWhat would you like to do?\n\n1. Create a customer \n2. View all customers \n3. Update a customer \n4. Delete a customer\n5. Exit\n\nNumber of action to run:"
-  );
-  const userSelection = prompt();
+  while (isActive) {
+    // Prompt
+    const prompt = require("prompt-sync")();
+    console.log(
+      "\nWhat would you like to do?\n\n1. Create a customer \n2. View all customers \n3. Update a customer \n4. Delete a customer\n5. Exit\n\nNumber of action to run:"
+    );
+    const userSelection = prompt();
 
-  switch (userSelection) {
-    case "1":
-      // User selects 1 to create a new customer
-      console.log(
-        "\nAdding a new customer\n\nWhat is the customer's new name?"
-      );
-      const newCustName = prompt();
-      console.log("What is the customer's age?");
-      const newCustAge = prompt();
-      const newCustDetails = {
-        name: newCustName,
-        age: Number(newCustAge),
-      };
-      await Customer.create(newCustDetails);
-      connect();
+    switch (userSelection) {
+      case "1":
+        // User selects 1 to create a new customer
+        console.log(
+          "\nAdding a new customer\n\nWhat is the customer's new name?"
+        );
+        const newCustName = prompt();
+        console.log("What is the customer's age?");
+        const newCustAge = prompt();
+        const newCustDetails = {
+          name: newCustName,
+          age: Number(newCustAge),
+        };
+        await Customer.create(newCustDetails);
+        break;
 
-    case "2":
-      // User selects 2 to view all customers
-      const viewAll = await Customer.find({});
-      console.log(viewAll);
-      connect();
+      case "2":
+        // User selects 2 to view all customers
+        const viewAll = await Customer.find({});
+        console.log(viewAll);
+        break;
 
-    case "3":
-      console.log(await Customer.find({}));
+      case "3":
+        console.log(await Customer.find({}));
+        console.log(
+          "Copy and paste the id of the customer you would like to update here: "
+        );
+        const updatingID = prompt();
+        console.log("What is the customer's new name?");
+        const updateCustName = prompt();
+        console.log("What is the customer's new age?");
+        const updateCustAge = prompt();
 
-      console.log(
-        "Copy and paste the id of the customer you would like to update here: "
-      );
-      const updatingID = prompt();
-      console.log("What is the customer's new name?");
-      const updateCustName = prompt();
-      console.log("What is the customer's new age?");
-      const updateCustAge = prompt();
+        await Customer.findByIdAndUpdate(
+          updatingID,
+          { name: updateCustName, age: updateCustAge },
+          { new: true }
+        );
+        break;
 
-      await Customer.findByIdAndUpdate(
-        updatingID,
-        { name: updateCustName, age: updateCustAge },
-        { new: true }
-      );
-      connect();
-
-    case "4":
-      console.log(await Customer.find({}));
-      console.log(
-        "Copy and paste the id of the customer you would like to delete here: "
-      );
-      const deletingID = prompt();
-      console.log("Confirm? [Y/N]");
-      const confirmation = prompt();
-      if (confirmation === "Y") {
-        await Customer.findByIdAndDelete(deletingID);
-      }
-      connect();
-
-    case "5":
-      console.log("... exiting application");
-      break;
+      case "4":
+        console.log(await Customer.find({}));
+        console.log(
+          "Copy and paste the id of the customer you would like to delete here: "
+        );
+        const deletingID = prompt();
+        console.log("Confirm? [Y/N]");
+        const confirmation = prompt();
+        if (confirmation === "Y") {
+          await Customer.findByIdAndDelete(deletingID);
+        }
+        break;
+      case "5":
+        console.log("... exiting application");
+        isActive = false;
+        break;
+    }
   }
 };
 
 connect();
-
-// const prompt = require("prompt-sync")();
-// const username = prompt("What is your name? ");
-// const userSelection = prompt(
-//   "\nWhat would you like to do?\n\n1. Create a customer \n2. View all customers \n3. Update a customer \n4. Delete a customer\n5. Exit\n\nNumber of action to run:\n"
-// );
-
-// if (userSelection === 1) {
-// }
-
-// // console.log(`Your name is ${userSelection}`);
